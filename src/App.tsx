d605fde01120c1393959dfba4a05ae37449d4907
Giovana O. Silva <giothais.silva@gmail.com>
2024-08-10 11:11:58 -0300

    feat: adicionando elementos a tela

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
 function App() {
-  return <>Olá mundo</>;
+  return (
+    <>
+      <div className="container">
+        <p className="text-type-one">365</p>
+        <p className="text-type-two">Motivos para</p>
+        <p className="text-type-three">amar a</p>
+        <p className="text-type-four">Meizinha</p>
+
+        <div className="button">
+          <button>
+            Clique aqui
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
